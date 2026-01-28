27d4832feef4ab5cc3e5ad058c0e10508a914f04
Claudia Richoux <c@laudiacay.cool>
2026-01-28 18:26:37 -0500

    Fix fwts formula: use pip_install to get dependencies

--- Formula/fwts.rb
+++ Formula/fwts.rb
@@ -1,6 +1,4 @@
 class Fwts < Formula
-  include Language::Python::Virtualenv
-
   desc "Git worktree workflow manager for feature development"
   homepage "https://github.com/laudiacay/featurebox"
   url "https://files.pythonhosted.org/packages/source/f/fwts/fwts-0.1.0.tar.gz"
@@ -10,10 +8,13 @@ class Fwts < Formula
   depends_on "python@3.11"
 
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.11")
+    venv.pip_install "fwts==#{version}"
+    bin.install_symlink libexec/"bin/fwts"
+    bin.install_symlink libexec/"bin/fb"
   end
 
   test do
-    system "#{bin}/fwts", "--version"
+    system bin/"fwts", "--help"
   end
 end
