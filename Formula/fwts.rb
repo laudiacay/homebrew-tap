3d4dd4812272397f65feffdabed8bede3c2b1336
Claudia Richoux <c@laudiacay.cool>
2026-01-30 18:23:04 -0500

    fwts: update to 0.1.18
    - Dynamic TUI resize support
    - Improved cleanup with force confirmation
    - Fix slash-to-dash in worktree paths

--- Formula/fwts.rb
+++ Formula/fwts.rb
@@ -3,8 +3,8 @@ class Fwts < Formula
 
   desc "Git worktree workflow manager for feature development"
   homepage "https://github.com/laudiacay/featurebox"
-  url "https://files.pythonhosted.org/packages/2a/7f/80a9fbb040b63d9773a82424a292c9ff70072fd3152f2ac07a9258e159dd/fwts-0.1.16.tar.gz"
-  sha256 "4d00ee97167c8849eab2d2ac208c16d17286092dce264f5ee5b08aa3c77fced8"
+  url "https://files.pythonhosted.org/packages/6d/84/809fda21ecf498b86ee0d256b69e5d0d2148bf9b3a91cbf717cb99d1fcea/fwts-0.1.18.tar.gz"
+  sha256 "cd89691851454d9c8d0702d426af156a35e52d2b720e53e076403c8fbd341443"
   license "MIT"
 
   depends_on "python@3.11"
